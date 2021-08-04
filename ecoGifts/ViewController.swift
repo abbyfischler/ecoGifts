892947c5438a73767839ac9531daf35234b0564f
Abby Fischler <abigail.fischler@gmail.com>
2021-08-03 20:49:39 -0700

    Added storyboards for each price ranges and added cocotouchclasses for each

--- ecoGifts/ViewController.swift
+++ ecoGifts/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func fiveToTen(_ sender: UIButton) {
+    }
+    
+    @IBAction func tenToTwentyFive(_ sender: UIButton) {
+    }
+    @IBAction func twentyFivetofifty(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiftyToSeventyfive(_ sender: UIButton) {
+    }
+    
+    @IBAction func seventyFivePlus(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
