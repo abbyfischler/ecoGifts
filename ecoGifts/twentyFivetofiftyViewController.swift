9d33ed7c892619a3d3f14dc8fbf37d6e9363fcbe
Abby Fischler <abigail.fischler@gmail.com>
2021-08-04 13:54:49 -0700

    Re did backarrows added 25-50code

--- ecoGifts/twentyFivetofiftyViewController.swift
+++ ecoGifts/twentyFivetofiftyViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class twentyFivetofiftyViewController: UIViewController {
 
-    @IBAction func hydroFlask(_ sender: Any) {
+    @IBAction func hydroFlask(_ sender: UIButton) {
         if let url = URL(string: "https://www.hydroflask.com/21-oz-standard-mouth?color=rain&gclid=CjwKCAjw9aiIBhA1EiwAJ_GTSjYnkhLeFdMttY5e9gvkArO91hPhHhQBFNKi67UxbYRqQFAG5YueqRoC4gcQAvD_BwE") {
                     UIApplication.shared.open(url, options: [:])
                 }
     }
+    
+    @IBAction func noteBook(_ sender: UIButton) {
+        if let url = URL(string: "https://www.etsy.com/listing/955205063/hardcover-100-recycled-paper-journal?ref=internal_similar_listing_bot-1") {
+                    UIApplication.shared.open(url, options: [:])
+                }
+    }
+    
+    @IBAction func bentoBox(_ sender: UIButton) {
+        if let url = URL(string: "https://ecologicalmarket.com/collections/on-the-go/products/bento-wet-box-large-rectangle") {
+                    UIApplication.shared.open(url, options: [:])
+                }
+    }
+    
+    @IBAction func soyWaxCandels(_ sender: UIButton) {
+        if let url = URL(string: "https://www.etsy.com/listing/924071902/bestseller-set-of-4-candles-35-oz?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=zero+waste+candles&ref=sc_gallery-1-4&plkey=abc1bc60ac115d3539249bf42a7cb7ec98fc9ba4%3A924071902&pro=1") {
+                    UIApplication.shared.open(url, options: [:])
+                }
+    }
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
